de0801b8b48a7bfaea58560ec57369dcaa8a1535
Macy <59153195+Macymo@users.noreply.github.com>
2020-08-04 16:29:55 -0400

    header/footer

--- netfling/src/App.js
+++ netfling/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+
+      <Footer />
     </div>
   );
 }
---
+++ netfling/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+// import "../styles/footer.css"
+
+function Footer() {
+    return (
+    <footer className="footer">
+        <div className="col-md-12">
+            <p role= "img" id="team">Made by SBLM<span role="img" aria-label="jsx-a11y/accessible-emoji">❤️</span>
+            </p>
+            <p id="year">2020
+            </p>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
