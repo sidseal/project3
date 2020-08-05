44440b2d50941ae905a2c423fa97e50a780b3862
Laura <lhernan53112@gmail.com>
2020-08-05 16:25:13 -0400

    imported store provider to app and sign up page

--- netfling/src/pages/signUp.js
+++ netfling/src/pages/signUp.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { EmailInput, PasswordInput, FormBtn } from "../components/Form";
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_USER} from "../../utils/actions";
+// import API from "../../utils/API";
 
 function SignUp() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const [state, dispatch] = useStoreContext();
+
     return (
         <form>
         <EmailInput />
