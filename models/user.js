481a19f13bfb4b0885407bcfb7f8fac415f360f8
Laura <lhernan53112@gmail.com>
2020-08-05 15:04:59 -0400

    Added globalState

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-  genderpreference: { type: String, required: true},
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  // name: { type: String, required: true },
+  // age: { type: Number, required: true },
+  // genderpreference: { type: String, required: true},
   img: { type: String },
   shows: [{type: String}]
 });
