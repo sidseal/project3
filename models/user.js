f3c0595484ab8e392c64e75f3802c0fa19202ec0
Laura <lhernan53112@gmail.com>
2020-08-06 17:42:58 -0400

    Uncomitted properties

--- models/user.js
+++ models/user.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  // name: { type: String, required: true },
-  // age: { type: Number, required: true },
-  // genderpreference: { type: String, required: true},
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  genderpreference: { type: String, required: true},
   img: { type: String },
   shows: [{type: String}]
 });
