64cd4830df0131b639f9db2479b66b47953b735e
Laura <lhernan53112@gmail.com>
2020-08-06 19:49:11 -0400

    Added SignUp and login state

--- netfling/src/App.js
+++ netfling/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-// import Login from "./pages/login";
+import Login from "./pages/login";
 import './App.css';
-// import { StoreProvider } from "./utils/GlobalState";
-import CreateProfile from './pages/createProfile';
-// import Choices from './components/Choices';
-
 
 function App() {
+  // Current User
+  const [loggedUser, setLoggedUser] = useState(
+    {
+      _id: 0,
+      email: "",
+      password: "",
+    }
+  );
+  
+
   return (
     <div className="App">
+     
       <Header />
-          <CreateProfile />
-        {/* <Login /> */}
+      
+      <Login setLoggedUser={setLoggedUser} loggedUser={loggedUser}/>
+
       <Footer />
+   
+
     </div>
   );
 }
