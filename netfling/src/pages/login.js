be7df7b1dbc4cd0b5fe8edb7d3c91192de7025e3
Laura <lhernan53112@gmail.com>
2020-08-06 17:41:06 -0400

    Refactored login page and added event listerners

--- netfling/src/pages/login.js
+++ netfling/src/pages/login.js
@@ -1,14 +1,62 @@
 import React from "react";
-import { EmailInput, PasswordInput, FormBtn } from "../components/Form";
+// import { EmailInput, PasswordInput, FormBtn } from "../components/Form";
 
-function Login() {
-    return (
-        <form>
-        <EmailInput />
-        <PasswordInput />
-        <FormBtn name="Login"/>
-        </form>
-    );
+function Login({ setLoggedUser, loggedUser }) {
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setLoggedUser({ ...loggedUser, [name]: value })
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // api call to confirm user
+
+    // route to profile
+    
+    console.log("its in the handlesubmit event ");
+    // find way to set unique id, returns as undefined
+    console.log("username is " + loggedUser.id);
+    console.log("username is " + loggedUser.email);
+    console.log("password is " + loggedUser.password);
+  };
+
+  return (
+    <>
+      <div className="row">
+        <div className="input-field col s12">
+          <input
+          onChange={handleInputChange}
+            id="email"
+            type="text"
+            className="validate"
+            name="email"
+          />
+          <label >Email</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input
+          onChange={handleInputChange}
+            id="password"
+            type="password"
+            className="validate"
+            name="password"
+          />
+          <label >Password</label>
+        </div>
+      </div>
+      <button className="btn waves-effect waves-light"
+        type="submit"
+        name="action"
+        onClick={handleSubmit}
+      >Login
+             <i className="material-icons right">send</i>
+      </button>
+    </>
+  );
 }
 
 export default Login;
