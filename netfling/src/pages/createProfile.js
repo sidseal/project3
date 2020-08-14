d3a1ab427f8e27f42d57dcd310543f25494692ce
Macy <59153195+Macymo@users.noreply.github.com>
2020-08-13 21:50:11 -0400

    dropdowns/buttons

--- netfling/src/pages/createProfile.js
+++ netfling/src/pages/createProfile.js
@@ -1,48 +1,78 @@
 import React from "react";
-// import User from "../components/Create";
-// import ShowChoices from "../components/Choices"
+import { User } from "../components/Create";
+import ShowChoices from "../components/Choices"
 import shows from "../../src/shows.json";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import Axios from "axios";
 import "../styles/createProfile.css";
 // import { DropdownList, Dropdown} from "../components/Create";
 
-let GenderPrefArr = [
+let UserGenderPrefArr = [
   {
     id: 1,
-    name: "Female interested in Male"
+    name: "Female"
   },
   {
     id: 2,
-    name: "Male interested in Female"
+    name: "Male"
   },
   {
     id: 3,
-    name: "Female interested in Female"
+    name: "Both"
+  }
+]
+
+const UsersGender = [
+  {
+    id: 1,
+    name: "Female"
   },
   {
-    id: 4,
-    name: "Male interested in Male"
-  },
-  {
-    id: 5,
-    name: "None"
-  },
+    id: 2,
+    name: "Male"
+  }
 ]
 
 function CreateProfile({ setLoggedUser, loggedUser }) {
-    
+
   console.log({ setLoggedUser, loggedUser })
 
   // Filtered Arrays
   const Series = shows.filter(series => series.type === "Series");
   const Movies = shows.filter(movie => movie.type === "Movie");
-  
+
   return (
     <div>
-       {/* <User placeholder="create username here"/> */}
+      <User placeholder="create username here" />
+
+      <DropdownButton id="dropdown-basic-button" variant="danger" title="Your Gender">
+        {UsersGender.map(gender =>
+          <Dropdown.Item
+            as="button"
+            key={gender.id}
+            name={gender.name}
+          >
+            {gender.name}
+          </Dropdown.Item>
+        )}
+      </DropdownButton>
+
+      {/* Gender Pref Dropdown */}
+      <DropdownButton id="dropdown-basic-button" variant="danger" title="Your Preference">
+        {UserGenderPrefArr.map(preference =>
+          <Dropdown.Item
+            as="button"
+            key={preference.id}
+            name={preference.name}
+          >
+            {preference.name}
+          </Dropdown.Item>
+        )}
+      </DropdownButton>
+
+      <p>Choose your top 4 and get matched</p>
 
       {/* series Dropdown */}
       <DropdownButton id="dropdown-basic-button" variant="danger" title="Choose Netflix series">
@@ -70,18 +100,26 @@ function CreateProfile({ setLoggedUser, loggedUser }) {
         )}
       </DropdownButton>
 
-      {/* Gender Pref Dropdown */}
-      <DropdownButton id="dropdown-basic-button" variant="danger" title="Gender Preference">
-        {GenderPrefArr.map(preference =>
-          <Dropdown.Item
-            as="button"
-            key={preference.id}
-            name={preference.name}
-          >
-            {preference.name}
-          </Dropdown.Item>
-        )}
-      </DropdownButton>
+      <div className="card">
+        <div className="card-image waves-effect waves-block waves-light">
+          <img className="activator" src="https://avatars1.githubusercontent.com/u/59153195?s=460&u=5c4f0554fbecf573645c785ef5ef66db1524bf8b&v=4" id="thumbnail" alt="profilepic" ></img>
+        </div>
+        <div className="card-content">
+          <p> <ShowChoices shows={shows.filter(show =>
+            show.id === 2
+          )}
+          />
+          </p>
+        </div>
+      </div>
+      <Link to={"/profile"}>
+        <button
+          className="btn waves-effect waves-light"
+          type="submit"
+          name="action">
+          Create My Profile!</button>
+      </Link>
+
 
     </div>
 
